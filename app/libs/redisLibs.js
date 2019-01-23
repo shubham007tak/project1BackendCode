a5db38c370cd8172b4df205efb9bc0621e258e63
shubham007tak <42111483+shubham007tak@users.noreply.github.com>
2019-01-23 16:49:04 +0530

    Update redisLibs.js

--- app/libs/redisLibs.js
+++ app/libs/redisLibs.js
@@ -3,7 +3,7 @@ const check = require("./checkLib.js");
 const redis = require('redis');
 let client = redis.createClient({
     url:'//redis-13499.c90.us-east-1-3.ec2.cloud.redislabs.com:13499',
-    password: 'Ram@1994',
+    password: '',
     port: 13499
 });
 
